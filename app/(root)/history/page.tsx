df6021ce1952cd8300117b7d01cd3f3c51a28c26
PiotrKociołek <piotrkociolek2002@gmail.com>
2023-08-19 11:31:11 +0200

    transfer to pc, without debug

---
+++ app/(root)/history/page.tsx
@@ -0,0 +1,5 @@
+const History = ()=>{
+    return <>Historia</>
+}
+
+export default History
---
+++ app/_components/zbikImage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Image} from "@nextui-org/react";
+
+
+
+const ZbikImage () => {
+    return (
+        <Image
+            width={300}
+            height={200}
+            alt="NextUI hero Image with delay"
+            src="./kotek.jpg"
+        />
+    );
+}
+
+export default ZbikImage
